2c6028815c47bc4e3ea14c23204ebbe5f83d78f0
Joel Huffman <huffmanj25@gmail.com>
2016-12-17 15:31:20 -0500

    styling

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,12 @@ module ArticlesHelper
   def article_params
     params.require(:article).permit(:title, :body, :tag_list, :image)
   end
+
+  def image(article)
+    if (!article.blank?)
+      return article
+    else
+      return "https://c2.staticflickr.com/8/7284/9261126436_216b937a43_b.jpg"
+    end
+  end
 end
