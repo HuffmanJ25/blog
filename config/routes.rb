16b34dfb882fe6f45b1cc5c0c15e4ce5f087290b
Joel Huffman <huffmanj25@gmail.com>
2016-12-16 19:19:18 -0500

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root to: 'articles#index'
@@ -8,4 +10,8 @@ Rails.application.routes.draw do
   end
 
   resources :tags
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
