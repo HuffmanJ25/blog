fcab7694d8622265a539bccccf01b441e05324ac
Joel Huffman <huffmanj25@gmail.com>
2016-12-20 19:19:15 -0500

    complete index style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'articles#index'
   get 'sessions/new'
 
   get 'users/new'
 
-  root to: 'articles#index'
+
 
   resources :articles do
     resources :comments
