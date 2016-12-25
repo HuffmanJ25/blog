e822a6654543343933a1591faa7b19f4a9de3659
Joel Huffman <huffmanj25@gmail.com>
2016-12-25 18:20:34 -0500

    adjust grid layouts

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -22,11 +22,11 @@ module ArticlesHelper
   def col_size(index)
     len = @articles.length
      if index == len-1
-            "9"
+            "mdl-card-wide mdl-cell mdl-cell--9-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-shadow--4dp"
      elsif index == len-2
-            "3"
+            "mdl-card-wide mdl-cell mdl-cell--3-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-shadow--4dp"
      else
-            "4"
+            "mdl-card-wide mdl-cell mdl-cell--4-col-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone mdl-shadow--4dp"
       end
   end
 
